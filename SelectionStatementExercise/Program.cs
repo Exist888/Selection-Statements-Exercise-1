ed1e8357d302b4dbf38b43247f7be608a313fc63
Filip Herbst <filip_herbst@yahoo.com>
2024-08-13 14:29:15 -0500

    Selection Statements Exercise 1

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,7 +4,47 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            
+            //Initialize Variables
+            var r = new Random();
+            var favNumber = r.Next(1, 21);
+            int userInput;
+            
+            //Prompt User to Guess Number
+            Console.WriteLine("Can you guess my favorite number?\n" +
+                              "It is between 1 and 20.\n" +
+                              "Give it a try:");
+            
+            //Receive User Input
+            //Create a Loop to Repeat Prompt if User is Incorrect
+            //Create If Else If Else to Reply to User Input
+            do
+            {
+                userInput = int.Parse(Console.ReadLine());
+            
+                if (userInput > favNumber)
+                {
+                    Console.WriteLine("You answer is too high!\n" +
+                                      "Try again:");
+                }
+                
+                else if (userInput < favNumber)
+                {
+                    Console.WriteLine("Your answer is too low!\n" +
+                                      "Try again:");
+                }
+
+                else
+                {
+                    Console.WriteLine("Nice! You guessed correctly!\n" +
+                                      "You get a trophy.");
+                }
+                
+            } while (favNumber != userInput);
+           
+            
+            
+            
         }
     }
 }
